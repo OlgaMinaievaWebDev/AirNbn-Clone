a491a274b6d76a6af472bef27eff4f6018765445
Olga <minaeva9@gmail.com>
2024-03-15 13:53:58 -0400

    card modified

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,14 +4,14 @@ import star from './assets/star.png'
 export default function Card() {
  return (
    <div className="card">
-     <img src={katie} className="card--image" />
+     <img src={katie} className="card--image" alt='main card image'/>
      <div className="card--stats">
-       <img src={star} className="card--star" />
+       <img src={star} className="card--star" alt='star icon' />
        <span>5.0</span>
        <span className="grey">(6) • </span>
        <span className="grey">USA</span>
      </div>
-     <p>Life Lessons with Katie Zaferes</p>
+     <h2>Life Lessons with Katie Zaferes</h2>
      <p>
        <span className="bold">From $136 </span>/ person
      </p>
